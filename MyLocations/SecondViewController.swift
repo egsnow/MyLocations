74f4f2c07a45f77df06069d70a52a60ef131df7a
Eric Snow <ericsnow@Kristinas-MacBook-Pro.local>
2019-02-24 19:09:57 -0600

    created basic framework of GPS locator app using tab bar setup, layout, get GPS cooridinates, CoreLocation to get user's current location and display information on screen

---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Eric Snow on 2/24/19.
+//  Copyright © 2019 Eric Snow. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
